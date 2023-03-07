c3ae54b53ff1399d6c8cc41f86824fffdb62ae8f
Alex Martinez <Alex@connectionlost.org>
2023-03-07 17:23:50 -0600

    Added social links in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,8 +48,8 @@ export const MyNavbar = () => {
               </Nav>
               <span className="navbar-text">
                 <div className="social-icon">
-                  <a href="#"><img src={navIcon1} alt="" /></a>
-                  <a href="#"><img src={navIcon2} alt="" /></a>
+                  <a href="https://www.linkedin.com/in/alexanderlmartinez/"><img src={navIcon1} alt="" /></a>
+                  <a href="https://www.instagram.com/alexlianthone/"><img src={navIcon2} alt="https://github.com/almartinez3356" /></a>
                   <a href="#"><img src={navIcon3} alt="" /></a>
                 </div>
                 <HashLink to='#connect'>
